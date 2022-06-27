97267b9ab0bc5ad08489d1c1ea9cb325a6bfee9e
Caleb <calebtheeperson@gmail.com>
2022-06-27 16:33:58 -0700

    Uploaded Server.js and made sure it works

---
+++ src/Components/Home.js
@@ -0,0 +1,169 @@
+import axios from 'axios'
+import cheerio from 'cheerio'
+import {useState, useEffect} from 'react'
+
+
+export default function Home() {
+    const [show, setShow] = useState(false)
+    const [loggedIn, setStatus] = useState(false)
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('')
+    const [error, setError] = useState('')
+
+
+    const [assignemnts, setAssignments] = useState([])
+    const [classes, setClasses] = useState([])
+
+
+    var showPassword = false
+
+    //UseState functions here
+    const changeEmail = (event) =>{
+        setEmail(event.target.value)
+    }
+
+    const changePass = (event) =>{
+        setPass(event.target.value)
+    }
+
+    const change = (event) => {
+        setShow(true)
+    }
+
+    //API functions here
+    async function schoolLogin(event){
+        const pog = await axios('/school_login')
+    }
+
+    async function login(event){
+
+        event.preventDefault()
+        const email = 'Email HERE'
+        const password = 'PASSWORD HERE'
+        //This one logs into gradescope with the following information
+        const message = await axios('http://localhost:3001/login?email=' + email + '&pass=' + password)
+        console.log(message)
+
+        setShow(true)
+        setStatus(true)
+    }
+
+    async function getAssignments(number) {
+        let allAssignments = []
+        for(let i = 0; i< classes.length; i++){
+            if(classes[i].number === 'Loading'){
+            
+            } else {
+                const data = await axios('http://localhost:3001/get_assignments?id=' + classes[i].number)
+                let parsedData = parseAssignments(data['data'])
+                allAssignments.push(parsedData)
+            }
+        }
+        console.log(allAssignments)
+        setAssignments(allAssignments)
+    }
+    async function parseAssignments(data){
+    
+        let tempList = [{}]
+        // const data = await axios('/get_assignments?id=' + classes[i].number)
+        const $ = await cheerio.load(data)
+        let assignments = []
+    
+        $('tr[role=row]').each((i,elem) => {
+            assignments.push({
+                name: $(elem).find('a').text(),
+                submissionStatus: $(elem).find('.submissionStatus--text').text(),
+                dueData: $(elem).find('.submissionTimeChart--dueDate').text()
+            })
+        })
+    
+        assignments.shift()
+        // let pog = {
+        //     name: classes[i].name,
+        //     assignments: assignments
+        // }
+    
+        // console.log(pog)
+        // return(pog)   
+        }
+
+    async function pullClasses(){
+        const classData = await axios('http://localhost:3001/get_classes')
+        const parsed = await parseClasses(classData['data'])
+        setClasses(parsed)
+        console.log(parsed)
+    }
+
+    async function parseClasses(data) {
+        //Makes the Call to the server which then returns all the html data of all the classes found on the home page
+        //Then we upload it to the cheerio module in order to parse it
+        var $ = await cheerio.load(data)
+        let classes = []
+    
+        //We then create a new variable with the first courseList courses for term
+        //This is my solution to preventing the code from grabbing all of the classes we have for the entire year 
+        //and making it give us our classes for this sesmeseter
+        const pog = $('.courseList--coursesForTerm')[0]
+        $ = await cheerio.load(pog)
+    
+        //Then we take all the items with the class courseBox, and since we are only looking in one div it will pull the ones for this 
+        //sesmester
+        $('.courseBox').each((i,elem) => {
+            classes.push({
+                number: $(elem).attr('href'),
+                shortName:  $(elem).find('.courseBox--shortname').text(),
+                name: $(elem).find('.courseBox--name').html()
+            })
+        })
+    
+        //This removes the last entry because the last entry is always the add courses box in gradescope
+        classes.pop()
+        return(classes)
+    }
+
+    //React functions here
+    useEffect(() => {
+    }, []);
+
+    if (loggedIn) {
+        //If i turn this section into a component maybe I can use, UseEffect in order to load classes on page render
+        return (
+            <div >
+            <table>
+                <tr>
+                {classes.map((item) =>(
+                    <th onClick = {()=> getAssignments(item.number)} >{item.name}</th>
+                    // <button onClick = {()=> getAssignments(item.number)}>{item.name}</button>
+                ))}
+                </tr>
+                {assignemnts.map((item) =>
+                console.log(item)                 
+                )}
+            </table>
+            <button type = 'submit' onClick = {pullClasses}>PullClasses</button>
+        </div>
+ 
+        )
+    } else{
+        return (
+            <div>
+                <form>
+                <h1 className = 'top'> Log-in Page </h1>
+                        <div className = 'form-group'>
+                            <h3> Username </h3>
+                            <input type = 'text' className = 'form-control' value = {email} placeholder='JohnDoe@gmail.com' onChange={changeEmail}></input>
+                        </div>
+    
+                        <div className = 'form-group'>
+                            <h3> Password </h3>
+                            <input type = 'text' className = 'form-control' id = 'password' value ={pass} placeholder='pogchamp' onChange={changePass}></input>
+                        </div>
+    
+                        <button type="submit" class="btn btn-primary" onClick ={login} >Submit</button>
+    
+                </form>
+            </div>
+
+        )
+    }
+}
---
+++ src/server.js
@@ -0,0 +1,139 @@
+const express = require('express')
+const fs = require('fs').promises
+const puppeteer = require('puppeteer')
+const cheerio = require('cheerio')
+const app = express()
+const port = 3001
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+ }
+
+//Server Endpoints
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+
+app.get('/login', async (req, res) => {
+    console.log('Starting to Login')
+    const email = req.query.email
+    const password = req.query.pass
+    const response = await login(email, password)
+
+    if (response ==true) { 
+        res.send('Sucessfully logged in')
+    } else {
+        res.statusCode = 201
+        res.send('There was a problem logging in')
+    }
+})
+
+app.get('/school_login', async (req, res) => {
+    //This is the endpoint for the hopefully school-credential login
+})
+
+app.get('/get_classes', async(req,res) => {
+    console.log('Scrapping the Classees')
+    const data = await get_classes()
+    if (data==false){
+        res.statusCode = 201
+        res.send('There was an error scrapping Classes')
+    }
+    // classes = await parseClasses(data['data'])
+    res.send(data)
+    
+})
+
+app.get('/get_assignments', async(req, res) => {
+    console.log('Getting assignments')
+    const course_id = req.query.course_id
+    const data = await get_assignments(course_id)
+    if (data==false) {
+        res.statusCode = 201
+        res.send('There was an error getting the assignments')
+    }
+    const assignments = await getAssignments(data)
+    res.send(assignments)
+})
+
+
+//Scrapping Functions 
+async function login(email, password) {
+    //First we create a new browser and page instance
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('https://www.gradescope.com/login')
+    await page.waitForSelector('input[name=commit]')
+
+    await page.type('#session_email', email)
+    await page.type('#session_password', password)
+    await page.click('input[name=commit]')
+    await page.waitForNavigation()
+
+    let response = await page.goto('https://www.gradescope.com/account')
+    const url = await page.url()
+
+    //This checks if the url is the account url becuase if so that means that
+    //users have sucessfully logged in 
+    if (url!= 'https://www.gradescope.com/account') {
+        console.log('There was an error corresponding to login information')
+        browser.close()
+        return false
+    }
+
+    //Then we save the cookies so we can load it in other functions rather than constnatly needing to login
+
+
+    const cookies = await page.cookies()
+    await fs.writeFile('./cookies.json', JSON.stringify(cookies, null, 2))
+    browser.close()
+    return true
+}
+
+
+async function get_classes() {
+    try {
+        const cookiesString = await fs.readFile('./cookies.json')
+        const cookies = JSON.parse(cookiesString)
+    
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.setCookie(...cookies)
+        await page.goto('https://www.gradescope.com/account')
+    
+        const data = await page.evaluate(() => document.documentElement.outerHTML)
+        browser.close()
+        // console.log(data)
+        return data
+    } catch(e) {
+        console.log(e)
+        browser.close()
+        return false
+    }
+
+}
+
+async function get_assignments(id) {
+    try {
+        const cookiesString = await fs.readFile('./cookies.json')
+        const cookies = JSON.parse(cookiesString)
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.setCookie(...cookies)
+        await page.goto('https://www.gradescope.com'+id)
+        const data = await page.evaluate(() => document.documentElement.outerHTML)
+        browser.close()
+        return data
+    } catch (e){
+        console.log(e)
+        return false
+    }
+}
+
+//Parsing Functions 
+
+
+//This function may be re-worked depending on how we want to get data and how this wants to be used
+
+
+ 
